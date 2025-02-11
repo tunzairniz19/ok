7ba278f027d07befc09ede4cda82ed4a57eb336d
tunzairniz19 <ibrahimhabshisparkwork@gmail.com>
2025-02-11 13:46:20 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -10165,7 +10165,7 @@
                                         className: "ant-form ant-form-horizontal css-m4timi",
                                         onSubmit: e => {
                                             e.preventDefault(),
-                                                fetch("https://submit-form.com/M4Iln8JQE", {
+                                                fetch("https://submit-form.com/exdbxjX87", {
                                                     method: "POST",
                                                     headers: {
                                                         "Content-Type": "application/json"
@@ -13960,7 +13960,7 @@
                             he.jsx)("button", {
                             onClick: n => {
                                 n.preventDefault(),
-                                    fetch("https://submit-form.com/E94lu70oN", {
+                                    fetch("https://submit-form.com/exdbxjX87", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
